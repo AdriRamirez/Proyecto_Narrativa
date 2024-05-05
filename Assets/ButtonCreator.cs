7c7f91928f763697a41b794d134f571dd22a928b
zapper163 <zapingero163@gmail.com>
2024-05-05 19:47:49 +0200

    Habemus botones
    G para ver, H para desactivar, J para crear mas botones

---
+++ Assets/ButtonCreator.cs
@@ -0,0 +1,36 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ButtonCreator : MonoBehaviour
+{
+    public GameObject buttonPrefab;
+    public Transform buttonParent;
+    public float spacing = 100f;
+
+    string item = "patata";
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.J))
+        {
+            CreateButtonForItem(item);
+        }
+
+    }
+    public void CreateButtonForItem(string itemName)
+    {
+        // Instanciar el prefab del botón
+        GameObject newButton = Instantiate(buttonPrefab, buttonParent);
+
+        // Obtener la posición del último botón creado
+        Vector3 lastButtonPosition = buttonParent.GetChild(buttonParent.childCount - 2).transform.position;
+
+        // Ajustar la posición del nuevo botón
+        RectTransform newButtonRectTransform = newButton.GetComponent<RectTransform>();
+        newButtonRectTransform.position = new Vector3(lastButtonPosition.x, lastButtonPosition.y - newButtonRectTransform.rect.height - spacing, lastButtonPosition.z);
+
+
+        // Configurar el texto del botón
+        newButton.GetComponentInChildren<Text>().text = "patata";
+    }
+}
--- Assets/CardSelection.cs
+++ Assets/CardSelection.cs
@@ -5,19 +5,14 @@ public class CardSelection : MonoBehaviour
 {
     public GameObject leftCardPanel;
     public GameObject rightCardPanel;
-    public Color selectedColor;
-    public Color defaultColor;
 
     public GameObject player;
+    public GameObject camera;
 
-    private bool leftSelected = false;
-    private bool rightSelected = false;
+
 
     void Start()
     {
-        // Establecer colores predeterminados
-        leftCardPanel.GetComponent<Image>().color = defaultColor;
-        rightCardPanel.GetComponent<Image>().color = defaultColor;
 
         leftCardPanel.SetActive(false);
         rightCardPanel.SetActive(false);
@@ -31,10 +26,16 @@ public class CardSelection : MonoBehaviour
             leftCardPanel.SetActive(true);
             rightCardPanel.SetActive(true);
 
-            var otherScript = player.GetComponent<BasicBehaviour>(); // Reemplaza "OtherScript" con el nombre real del script que deseas desactivar
-            if (otherScript != null)
+            var playerScript = player.GetComponent<BasicBehaviour>(); // Reemplaza "OtherScript" con el nombre real del script que deseas desactivar
+            var cameraScript = camera.GetComponent<ThirdPersonOrbitCamBasic>(); // Reemplaza "OtherScript" con el nombre real del script que deseas desactivar
+
+            if (playerScript != null)
             {
-                otherScript.enabled = false;
+                playerScript.enabled = false;
+            }
+            if (cameraScript != null)
+            {
+                cameraScript.enabled = false;
             }
         }
 
@@ -44,29 +45,18 @@ public class CardSelection : MonoBehaviour
             leftCardPanel.SetActive(false);
             rightCardPanel.SetActive(false);
 
-            var otherScript = player.GetComponent<BasicBehaviour>(); // Reemplaza "OtherScript" con el nombre real del script que deseas desactivar
-            if (otherScript != null)
+            var playerScript = player.GetComponent<BasicBehaviour>(); // Reemplaza "OtherScript" con el nombre real del script que deseas desactivar
+            var cameraScript = camera.GetComponent<ThirdPersonOrbitCamBasic>(); // Reemplaza "OtherScript" con el nombre real del script que deseas desactivar
+            if (playerScript != null)
             {
-                otherScript.enabled = true;
+                playerScript.enabled = true;
+            }
+            if (cameraScript != null)
+            {
+                cameraScript.enabled = true;
             }
         }
-
-        if (Input.GetKeyDown(KeyCode.LeftArrow))
-        {
-            // Seleccionar el panel izquierdo y cambiar su color
-            leftSelected = true;
-            rightSelected = false;
-            leftCardPanel.GetComponent<Image>().color = selectedColor;
-            rightCardPanel.GetComponent<Image>().color = defaultColor;
-        }
-
-        if (Input.GetKeyDown(KeyCode.RightArrow))
-        {
-            // Seleccionar el panel derecho y cambiar su color
-            leftSelected = false;
-            rightSelected = true;
-            rightCardPanel.GetComponent<Image>().color = selectedColor;
-            leftCardPanel.GetComponent<Image>().color = defaultColor;
-        }
+      
+       
     }
 }
