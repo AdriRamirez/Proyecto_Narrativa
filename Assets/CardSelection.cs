4bf81b6aeaf3b105aca7987aa4618ab9204e79cf
zapper163 <zapingero163@gmail.com>
2024-05-13 12:15:54 +0200

    Finished Cards

--- Assets/CardSelection.cs
+++ Assets/CardSelection.cs
@@ -1,30 +1,42 @@
+using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class CardSelection : MonoBehaviour
 {
-    public GameObject leftCardPanel;
-    public GameObject rightCardPanel;
+    public GameObject ElectionScreen;
+    public GameObject DiceScreen;
+
+    public GameObject numDado;
+    public GameObject yourResult;
 
     public GameObject player;
     public GameObject camera;
 
     public Transform cameraTarget;
 
+
+
     public bool isCardsTriggered = false;
+    bool Continue = true;
 
 
     void Start()
     {
 
-        leftCardPanel.SetActive(false);
-        rightCardPanel.SetActive(false);
+        ElectionScreen.SetActive(false);
+        DiceScreen.SetActive(false);
+
+        
+
     }
 
     private void OnTriggerStay(Collider other)
     {
-        if (other.gameObject.tag == "Player")
+        //Debug.Log("patata");
+        if (other.gameObject.tag == "Player" && Continue == true)
         {
+            Continue = false;
             StartCards();
 
         }
@@ -33,35 +45,19 @@ public class CardSelection : MonoBehaviour
 
     void Update()
     {
-        if (isCardsTriggered)
+        if (!isCardsTriggered)
         {
-            
-            leftCardPanel.SetActive(true);
-            rightCardPanel.SetActive(true);
+
+            ElectionScreen.SetActive(false);
+            DiceScreen.SetActive(false);
+
+
 
             var playerScript = player.GetComponent<FirstPersonMovement>();
             var cameraScript = camera.GetComponent<FirstPersonLook>();
 
-            if (playerScript != null)
-            {
-                playerScript.speed = 0;
-            }
-            if (cameraScript != null)
-            {
-                cameraScript.sensitivity = 0;
-                cameraScript.inDialog = false;
 
-            }
-        }
-
-        if(!isCardsTriggered)
-        {
-            
-            leftCardPanel.SetActive(false);
-            rightCardPanel.SetActive(false);
-
-            var playerScript = player.GetComponent<FirstPersonMovement>();
-            var cameraScript = camera.GetComponent<FirstPersonLook>();
+            Cursor.lockState = CursorLockMode.Locked;
             if (playerScript != null)
             {
                 playerScript.speed = 5;
@@ -73,12 +69,75 @@ public class CardSelection : MonoBehaviour
 
             }
         }
-      
-       
+
+
     }
 
     void StartCards()
     {
         isCardsTriggered = true;
+        if (isCardsTriggered)
+        {
+
+            Cursor.lockState = CursorLockMode.Confined;
+            ElectionScreen.SetActive(true);
+           
+
+            var playerScript = player.GetComponent<FirstPersonMovement>();
+            var cameraScript = camera.GetComponent<FirstPersonLook>();
+
+            if (playerScript != null)
+            {
+                playerScript.speed = 0;
+            }
+            if (cameraScript != null)
+            {
+                cameraScript.sensitivity = 0;
+                cameraScript.inDialog = true;
+                camera.transform.LookAt(cameraTarget);
+
+            }
+        }
+
+        
     }
+
+    public void GoToDiceScreen()
+    {
+        TextMeshProUGUI objectiveDice = numDado.GetComponent<TextMeshProUGUI>();
+
+        string objectiveNum = "10";
+        ElectionScreen.SetActive(false);
+        DiceScreen.SetActive(true);
+        objectiveDice.text = objectiveNum;
+    }
+
+    public void GoToDiceScreen2()
+    {
+
+        TextMeshProUGUI objectiveDice = numDado.GetComponent<TextMeshProUGUI>();
+
+        string  objectiveNum = "15";
+        ElectionScreen.SetActive(false);
+        DiceScreen.SetActive(true);
+        objectiveDice.text = objectiveNum;
+    }
+
+    public void RollDice()
+    {
+        int yourRoll = Random.Range(1, 20);
+
+        TextMeshProUGUI tirada = yourResult.GetComponent<TextMeshProUGUI>();
+
+        tirada.text = yourRoll.ToString();
+    }
+
+    public void EndCards()
+    {
+        isCardsTriggered = false;
+        Continue = false;
+    }
+
+
+
 }
