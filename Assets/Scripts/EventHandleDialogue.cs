06dda7d4c778db6d3a1cca6e89d70f44bfcb20b2
AdriRamirez <a99monster@gmail.com>
2024-06-06 10:53:01 +0200

    final

--- Assets/Scripts/EventHandleDialogue.cs
+++ Assets/Scripts/EventHandleDialogue.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using UnityEngine;
 using DialogueEditor;
 using UnityEngine.TextCore.Text;
-using static UnityEditor.Profiling.RawFrameDataView;
 
 public class EventHandleDialogue : MonoBehaviour
 {
